d3c7f10d9a35e88c1046dcb2dc2fe2a653b988d8
Artyom <prime.manticore@mail.ru>
2020-03-28 19:03:04 +0300

    API доступа к избранным комнатам

--- backend/paper_games/virus_war/urls.py
+++ backend/paper_games/virus_war/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('users/<int:pk>/',                UserRetrieve.as_view()),
     path('rooms/',                         RoomListCreate.as_view()),
     path('rooms/<int:pk>/',                RoomRetrieveUpdateDestroy.as_view()),
+    path('starred/',                       StarredCreate.as_view()),
+    path('starred/<int:room_id>/',         StarredDestroy.as_view()),
 ]
